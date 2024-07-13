f11244e768b9ec0531bc806fa9c09124001e1139
SebastianIdk2 <gsmarquez@puce.edu.ec>
2024-07-13 09:36:47 -0500

    Examen Parcial 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie (models.Model):
+    title = models.CharField(max_length=50, null=False)
+    gender = models.CharField(max_length=35, null=False)
+    director = models.CharField(max_length=50, null=False)
+    year = models.IntegerField(null=False)
+    synopsis = models.TextField(max_length=300, null=False)
+    
+    def __str__(self) -> str:
+        return self.title
